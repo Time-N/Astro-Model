39259f3774df8bd04333655b2ef8852179b5882c
eormes <ethanormes@gmail.com>
2025-10-02 14:16:08 -0600

    update platelistdata comments

--- platelistdata.m
+++ platelistdata.m
@@ -1,9 +1,12 @@
 % Processes the platelist.fits file and displays number
-% of quasars, galaxies, and stars.
+% of quasars, galaxies, and stars on the y axis and the
+% plate number on the x axis.
 info = fitsinfo('platelist.fits');
 disp(info);
 data = fitsread('platelist.fits', 'BINTABLE');
 plate = data{1};
+chunk = data{18};
+N_guide = data{65};
 N_total = data{88};
 N_galaxy = data{89};
 N_quasar = data{90};
