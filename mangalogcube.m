25608731d6a017da6e6af7f82ec374c13544f0f0
eormes <ethanormes@gmail.com>
2025-10-02 14:10:47 -0600

    Add mangalogcube and mangalogcube driver

---
+++ mangalogcube.m
@@ -0,0 +1,36 @@
+% Processes the manga-10001-12701-LOGCUBE.fits file and generates an
+% image of a spectrum of wavelengths
+
+info = fitsinfo('manga-10001-12701-LOGCUBE.fits');
+cube = fitsread('manga-10001-12701-LOGCUBE.fits','Image',1);
+wave = fitsread('manga-10001-12701-LOGCUBE.fits','binarytable',1);
+
+hdr = info.Image(1).Keywords;
+
+% Find the row index for each keyword
+idx_crval3 = find(strcmp(hdr(:,1),'CRVAL3'));
+idx_cdelt3 = find(strcmp(hdr(:,1),'CD3_3'));
+idx_naxis3 = find(strcmp(hdr(:,1),'NAXIS3'));
+
+% Extract numeric values safely
+crval3 = hdr{idx_crval3,2};
+cdelt3 = hdr{idx_cdelt3,2};
+naxis3 = hdr{idx_naxis3,2};
+
+%w1=3000;
+%w2=4000;
+
+% Compute the full wavelength array
+wave_full_log = crval3:0.8339:crval3+(size(cube,3)-1)*0.8339;
+
+% Find the corresponding indicies to real wavelength range in angstrom
+w_indices = find(wave_full_log >= (w1) & wave_full_log <=(w2));
+
+% Extract that range
+subcube = cube(:, :, w_indices);
+narrowband_image = squeeze(sum(subcube,3));
+imagesc(narrowband_image);
+colormap(jet);
+axis image;
+colorbar;
+title(sprintf('Sum over wavelengths %d-%d', w1, w2))
---
+++ mangalogcubedriver.m
@@ -0,0 +1,40 @@
+% Driver file for mangalogcube to generate N plots of evenly spaced
+% wavelengths
+
+N=36; % Number of subplots (must be square)
+
+dw = -(3.6216e+03 - 7.4258e+03)/N; % change in wavelength
+
+for n = 1:N
+    subplot(sqrt(N),sqrt(N),n)
+    w1 = 3.6216e+03 + dw*(n-1);
+    w2 = 3.6216e+03 + dw*n;
+    mangalogcube
+    hold on;
+end
+hold off
+
+
+%{
+subplot(2,2,1);
+w1=3000;
+w2=8000;
+mangalogcube;
+hold on;
+
+subplot(2,2,2);
+w1=5000;
+w2=5250;
+mangalogcube;
+
+subplot(2,2,3);
+w1=0;
+w2=4000;
+mangalogcube;
+
+subplot(2,2,4);
+w1=6730;
+w2=7850;
+mangalogcube;
+hold off
+%}
